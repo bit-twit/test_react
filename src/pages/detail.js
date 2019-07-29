e6d26689a6777b53b3550f8d5354ea37e8c441cf
Iulian Plesoianu <bit.twit@gmail.com>
2019-07-29 12:29:33 +0300

    Add list of values rendering

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -4,22 +4,43 @@ import ChanceSeed from 'chance';
 const Chance = ChanceSeed();
 
 class Detail extends React.Component {
-    constructor() {
-        super();
-        console.log(Chance);
+    constructor(props) {
+        super(props);
         console.log("Hi from constructor.");
+
+        const people = [];
+        for (let i = 0; i < 10; i++) {
+            people.push({
+                name: Chance.first(),
+                country: Chance.country({ full: true })
+            });
+        }
+        this.state = {
+            message: props.message,
+            people
+        };
     }
 
     buttonClicked() {
-        console.log('Button was clicked!')
+        console.log('Button was clicked!');
+        const newState = this.state.people;
+        newState.push({
+           name: Chance.first(),
+           country: Chance.country({ full: true })
+        });
+        this.setState(newState);
     }
 
     render() {
         return (<div>
-                <p>This is React rendering HTML {this.props.message}!</p>
-                <p>Hello {Chance.first()}!</p>
-                <p>You're from {Chance.country({ full: true })}.</p>
-                <button onClick={this.buttonClicked}>Meet Someone New</button>
+                <p>This is React rendering HTML {this.state.message}!</p>
+                <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
+                <br/>
+                <div style={{maxHeight: '300px', overflow: 'auto'}}>
+                {this.state.people.map((p, index) => (
+                    <p key={index}>Hello nr. {index}, {p.name}, from whatever {p.country} !</p>
+                ))}
+                </div>
             </div>);
     }
 }
