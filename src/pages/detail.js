9d1a4bc111a016a3a71da7cad080eac581455c2f
Iulian Plesoianu <bit.twit@gmail.com>
2019-07-29 11:38:08 +0300

    Fixed Chance import error

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import ChanceSeed from 'chance';
+
+const Chance = ChanceSeed();
 
 class Detail extends React.Component {
+    constructor() {
+        super();
+        console.log(Chance);
+        console.log("Hi from constructor.");
+    }
+
+    buttonClicked() {
+        console.log('Button was clicked!')
+    }
+
     render() {
-        return <p>This is React rendering HTML {this.props.message}!</p>;
+        return (<div>
+                <p>This is React rendering HTML {this.props.message}!</p>
+                <p>Hello {Chance.first()}!</p>
+                <p>You're from {Chance.country({ full: true })}.</p>
+                <button onClick={this.buttonClicked}>Meet Someone New</button>
+            </div>);
     }
 }
 
